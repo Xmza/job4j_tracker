9b8cb301cbdfcde9f223dbf5bb9a5e3d45fa9e6d
Xmza <mountain.xmz@gmail.com>
2023-09-07 21:55:13 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println("Ошибка активна: " + active);
+        System.out.println("Номер ошибки: " + status);
+        System.out.println("Содержание: " + message);
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 404, "Ошибка при запуске приложения");
+        error.printInfo();
+    }
+}
