8e6b91a2dce1383b015d8e3ca95ff24c95d3a9ec
Xmza <mountain.xmz@gmail.com>
2023-09-14 20:35:54 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,15 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+    @Override
+    public String generate(String name, String body) {
+        String ln = System.lineSeparator();
+        return "{" + ln
+                + "\t\"name\" : \"" + name + "\"," + ln
+                + "\t\"body\" : \"" + body + "\"" + ln
+                + "}";
+    }
+
     public static void main(String[] args) {
         TextReport report = new TextReport();
         System.out.println(report.generate("name", "body"));
