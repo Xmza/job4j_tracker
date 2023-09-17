cdc694673892f3de75987c9e65eab0d7fb1a715e
Xmza <mountain.xmz@gmail.com>
2023-09-17 21:17:15 +0300

    3.2. Удаление моделей из массива. [#310016]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -1,13 +1,11 @@
-//package ru.job4j.pojo;
-//
-//public class ShopDrop {
-//    public static Product[] delete(Product[] products, int index) {
-//        for (int i = 0; i < products.length; i++) {
-//            if (i == index) {
-//                products
-//            }
-//        }
-//
-//        return products;
-//    }
-//}
+package ru.job4j.pojo;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+             products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
