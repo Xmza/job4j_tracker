f79d40d484589c8e9a60654f2ff57019941cd453
Xmza <mountain.xmz@gmail.com>
2023-09-18 21:57:47 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,34 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int newSize = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item != null) {
+                rsl[newSize] = item;
+                newSize++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, newSize);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int newSize = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item.getName().equals(key)) {
+                rsl[newSize] = item;
+                newSize++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, newSize);
+        return rsl;
     }
 
     public Item findById(int id) {
