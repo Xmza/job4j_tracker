49ccb223424cd8a6483bf9d9956fceda93756699
Xmza <mountain.xmz@gmail.com>
2023-09-27 21:49:25 +0300

    3.2. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,8 +16,6 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select == 6) {
-                run = false;
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
@@ -29,6 +27,8 @@ public class StartUI {
                     System.out.println("Хранилище еще не содержит заявок");
 
                 }
+            } else if (select == 6) {
+                run = false;
             }
         }
     }
