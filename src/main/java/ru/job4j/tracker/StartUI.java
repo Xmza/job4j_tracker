8c8cedcdf7cd5223bb53ca3796d0fcdb8f4880bf
Xmza <mountain.xmz@gmail.com>
2023-09-28 21:52:45 +0300

    3.0. Реализация класса StartUI. Вывод меню [#500743 #448302]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,43 +9,9 @@ public class StartUI {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select == 0) {
-                System.out.println("=== Create a new Item ===");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                tracker.add(item);
-                System.out.println("Добавленная заявка: " + item);
-            } else if (select == 1) {
-                System.out.println("=== Show all items ===");
-                Item[] items = tracker.findAll();
-                if (items.length > 0) {
-                    for (Item item : items) {
-                        System.out.println(item);
-                    }
-                } else {
-                    System.out.println("Хранилище еще не содержит заявок");
-                }
-            } else if (select == 2) {
-                System.out.println("=== Edit item ===");
-                System.out.print("Ented id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                System.out.print("Entee name: ");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Заявка успешно изменена");
-                } else {
-                    System.out.println("Ошибка замены заявки");
-                }
-            } else if (select == 3) {
-                System.out.println("=== Delete item ===");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                tracker.delete(id);
-                Item item = tracker.findById(id);
-                System.out.println(item == null ? "Заявка удалена успешно." : "Ошибка удаления заявки.");
-            } else if (select == 6) {
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
                 run = false;
             }
         }
