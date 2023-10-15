f7f27952f1d5cf4862ab504ec870d9093bb1d355
Xmza <mountain.xmz@gmail.com>
2023-10-15 21:14:27 +0300

    0.4. Пользовательские исключения. [#219367 #452661]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,12 +1,6 @@
 package ru.job4j.ex;
 
-    class ElementNotFoundException extends Exception {
-        public ElementNotFoundException(String message) {
-            super(message);
-        }
-    }
-
-    public class FindEl {
+public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
