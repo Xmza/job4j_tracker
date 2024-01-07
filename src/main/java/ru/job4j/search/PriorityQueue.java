f07102c441d5a489d3aba58a5f3f49c4a043ee29
Xmza <mountain.xmz@gmail.com>
2024-01-07 07:59:35 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -9,8 +9,7 @@ public class PriorityQueue {
         int index = 0;
         for (Task element : tasks) {
             if (task.getPriority() < element.getPriority()) {
-                tasks.add(index, task);
-                return;
+               break;
             }
             index++;
         }
