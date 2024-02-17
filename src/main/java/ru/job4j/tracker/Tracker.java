dc5305d5b73c5175479ec305ab842d6514bb9522
Xmza <mountain.xmz@gmail.com>
2024-02-17 22:22:40 +0300

    9. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,43 +1,42 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    private final List<Item> items = new ArrayList<>();
     private int ids = 1;
     private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
-    public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+    public List<Item> findAll() {
+        return new ArrayList<>(items);
     }
 
-    public Item[] findByName(String key) {
-        Item[] result = new Item[size];
-        int newSize = 0;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (item.getName().equals(key)) {
-                result[newSize] = item;
-                newSize++;
+    public List<Item> findByName(String key) {
+        List<Item> result = new ArrayList<>();
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                result.add(item);
             }
         }
-        return Arrays.copyOf(result, newSize);
+        return result;
     }
 
     private int indexOf(int id) {
         for (int i = 0; i < size; i++) {
-            if (items[i].getId() == id) {
+            if (items.get(i).getId() == id) {
                 return i;
             }
         }
@@ -49,7 +48,7 @@ public class Tracker {
         boolean result = index != -1;
         if (result) {
             item.setId(id);
-            items[index] = item;
+            items.set(index, item);
         }
         return result;
     }
@@ -57,9 +56,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            System.arraycopy(items, index + 1, items, index, size - index - 1);
-            items[size - 1] = null;
-            size--;
+            items.remove(index);
         }
     }
 }
--- src/main/java/ru/job4j/tracker/action/FindByName.java
+++ src/main/java/ru/job4j/tracker/action/FindByName.java
@@ -5,6 +5,8 @@ import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Output;
 import ru.job4j.tracker.Tracker;
 
+import java.util.List;
+
 public class FindByName implements User {
     private final Output output;
 
@@ -21,8 +23,8 @@ public class FindByName implements User {
     public boolean execute(Input input, Tracker tracker) {
         output.println("=== Поиск заявок по имени ===");
         String name = input.askStr("Введите имя: ");
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
+        List<Item> items = tracker.findByName(name);
+        if (items.isEmpty()) {
             for (Item item : items) {
                 output.println(item);
             }
