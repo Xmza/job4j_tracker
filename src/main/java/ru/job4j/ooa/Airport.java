f3c12259a182811790450f12f3e2fac04b87ba30
Admin <mountain.xmz@gmail.com>
2024-03-08 22:39:51 +0300

    3. Модели данных и HashSet. [#218530]

--- src/main/java/ru/job4j/ooa/Airport.java
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -11,7 +11,6 @@ public class Airport {
         System.out.println(airbus);
         airbus.printModel();
         airbus.printCountEngine();
-
         airbus.setName("A380");
         System.out.println(airbus);
     }
