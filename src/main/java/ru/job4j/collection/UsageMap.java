0a3c1dda35a471a9f1bee0d00080579be1d577cd
Admin <mountain.xmz@gmail.com>
2024-03-15 21:21:02 +0300

    0. Map, HashMap [#212652 #485630]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] dasda) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("paveldurov@tg.ru", "Pavel Durov");
+        map.put("ingvarvoitenko@mail.ru", "Igor Voitenko");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
 
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String key = entry.getKey();
